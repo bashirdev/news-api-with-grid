6bb6a64a5d60d4ff059e92345529cc27a41cbf4a
bashir ahammed <bahammed16@gmail.com>
2020-08-30 21:16:10 +0400

    news site create it with news api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import News from './component/News/News';
+import Carditem from './component/Card/Card';
+import {Grid} from '@material-ui/core';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+   gridContainer:{
+  
+   }
+}));
 
 function App() {
+  const classes= useStyles();
+  const [articles,setArticles] = useState([]);
+
+ useEffect(()=>{
+    axios.get(' https://newsapi.org/v2/top-headlines?country=us&apiKey=d275cf2d75e84a1d85b2aa718654ea9e')
+    .then(data => setArticles(data.data.articles))
+ },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Grid container 
+     spacing={4}
+     direction="row"
+     justify="flex-space-between"
+    alignItems="flex-center">
+  
+   
+    {articles.map(article =>  <Grid item xs={12} sm={6} md={3}> <Carditem article={article} /> </Grid>)}
+    
+  
+   </Grid>
+    
+   <Grid container spacing={4}>
+    <Grid item xs={12} sm={6} md={4}>
+    <News />
+    </Grid>
+    <Grid item xs={12} sm={6} md={4}>
+    <News />
+    </Grid>
+    <Grid item xs={12} sm={6} md={4}>
+    <News />
+    </Grid>
+    <Grid item xs={12} sm={6} md={4}>
+    <News />
+    </Grid>
+   </Grid>
+
+    
+    </>
   );
 }
 
